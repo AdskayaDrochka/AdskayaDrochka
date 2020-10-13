d265fa83c2139358c55844241effe8d75057611c
user <user@vpmt.ru>
2020-10-13 12:27:37 +0300

    Добавьте файлы проекта.

---
+++ laba3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0;i<19;i++)
+            {
+                Console.WriteLine("ооооо нормально)))))))");
+            }
+            Console.ReadKey();
+        }
+    }
+}
